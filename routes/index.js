49c000254bd0b810a6245965b4b65cad33f87794
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-04-13 11:59:41 +0200

    Add: Request for changing light color

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const request = require('request')
+
+const deviceID = 'D999';
+const URL = 'https://oege.ie.hva.nl/~palr001/icu/api.php?t=sdc&d=D999&td=8EA1&c=ff0000';
 
 router.get('/', (req, res) => {
+	request(URL, function(error, response, body) {
+		if(error) throw error
+		console.log(body);
+	});
 	res.render('index')
-})
+});
 
 module.exports = router
