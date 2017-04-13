35491fd30a0300eb0eaddf5a67325318ee39cc1a
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-04-13 12:31:38 +0200

    Port for Heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-const port = 1337
+const port = process.env.PORT || 8080;
 
 const indexRouter = require('./routes/index.js')
 
