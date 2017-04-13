f5c9dd6910628831500772ed50547ed4c041209d
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-04-13 13:46:42 +0200

    Add: MongoDB with register and login functionality

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
+/* LOAD ALL DEPENDENCIES
+----------------------------------------- */
 //google calander api key AIzaSyB12EjnA2KlxqiDxHzXfmwzcz7bz8nLO4U
 const express = require('express')
 const path = require('path')
+const session = require('express-session')
+const bodyParser = require('body-parser')
 const app = express()
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
+/* MONGODB CONFIGURATION
+----------------------------------------- */
+const MongoClient = require("mongodb").MongoClient;
+require('dotenv').config();
+const dbConfig = process.env.MONGODB_URI;
+
+MongoClient.connect(dbConfig, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+});
+
+/* SESSIONS CONFIGURATION
+----------------------------------------- */
+app.use(session({
+    secret: "JA1d82JHYF9?nsdfDF635MuHe#ksd",
+    resave: false,
+    saveUninitialized: true
+}));
+
+/* BODY-PARSER FOR READING POST REQUESTS
+----------------------------------------- */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+/* Routes
+----------------------------------------- */
 const indexRouter = require('./routes/index.js')
+const accountRouter = require('./routes/account.js')
 
 app
 	.set('view engine', 'ejs')
 	.use(express.static('public'))
 	.use('/', indexRouter)
+	.use('/account', accountRouter)
 	.listen(port, () => {
 		console.log('Started server on http://localhost:' + port)
 	})
