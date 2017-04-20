88efab5e16e0c83a66392c12e60ee416947343a5
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-04-20 12:36:25 +0200

    Change:  Config in Lua files for nodeMCU

--- New NodeMCU/arduino/config.lua
+++ New NodeMCU/arduino/config.lua
@@ -1,7 +1,7 @@
 local config = {}
 
-config.ssid = 'INTERNET'
-config.password = 'Power112!'
+config.ssid = 'HvA Open Wi-Fi'
+config.password = ''
 config.server = 'e17d6e22.ngrok.io'
 config.port = '3000'
 
