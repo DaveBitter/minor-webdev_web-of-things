5eaf86545df92ef938fb9db4682e62bf8a3a09a9
d.bitter <daveybitter@gmail.com>
2017-05-02 16:23:42 +0200

    wip

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ function getSenior(senderId) {
               }, function(err, foundSenior) {
                 console.log(foundSenior)
                 console.log('emitting the color' + user.color + ' to foundSenior ' + foundSenior.boxId)
-                wss.broadcast(
+                ws.broadcast(
                   JSON.stringify({
                     color: user.color,
                     recipient: foundSenior.boxId
